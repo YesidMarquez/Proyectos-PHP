caf1de38fa747c7ac384b6c86ca2830b1f4952d0
YesidMarquez <Marquez2388@utp.edu.co>
2018-07-05 17:53:50 -0500

    ac

--- proyecto1/tip_cons/ver.php
+++ proyecto1/tip_cons/ver.php
@@ -14,7 +14,7 @@
     $row1 = $resultado1->fetch_array(MYSQLI_ASSOC);
    
      /*-----------------------------------------------------------------------*/
-    $sql2 = "SELECT cu.descripcion,cc.nro_curso,cc.fecha_vencimiento FROM control_cursos cc, cursos cu  WHERE cc.empleado_id='$id' and cc.curso_id=cu.id_cursos ";
+    $sql2 = "SELECT cu.descripcion,cc.nro_curso,cc.fecha_vencimiento, ac.nombre FROM control_cursos cc, cursos cu, academias ac  WHERE cc.empleado_id='$id' and cc.curso_id=cu.id_cursos and ac.nit_academia= cc.academia_nit";
     $resultado2 = $mysqli->query($sql2);
     $row2 = $resultado2->fetch_array(MYSQLI_ASSOC);
     /*------------------------------------------------------------------------*/
@@ -49,9 +49,17 @@
         $ruta3=$filas['ruta'];
         $desc3=$filas['id_imgcurso'];
     }
-/************************************************** FIN AREA CONEXION Y CONSULTA CON BD  (BACKEND)PROYECTO *******************/ 
+    /*------------------------------------------------------------------------*/
+    $sql4 = "SELECT * FROM contrato WHERE empleado_id = '$id' ";
+    $resultado4 = $mysqli->query($sql4);
+    $row4 = $resultado4->fetch_array(MYSQLI_ASSOC);
+    /*------------------------------------------------------------------------*/
+    $sql5 = "SELECT * FROM tipo_contrato, contrato WHERE empleado_id = '$id' and    tipo_contrato_id=   id_tipo_contrato ";
+    $resultado5 = $mysqli->query($sql5);
+    $row5 = $resultado5->fetch_array(MYSQLI_ASSOC);
+/************************************************** FIN AREA CONEXION Y CONSULTA CON BD  (BACKEND)PROYECTO ****************************/ 
 ?>
-<!--******************************************************** INICIO AREA DE DISEÑO (FRONTEND)PROYECTO *****************-->
+<!--******************************************************** INICIO AREA DE DISEÑO (FRONTEND)PROYECTO ********************************-->
 <!DOCTYPE html>
 <html>
 <head>
@@ -61,18 +69,18 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
-<body >
-    <div class="container-fluid">
+<body style="background-color:lavender;">
+    <!--<div class="container-fluid">
         <div class="row justify-content-md-center">
             <div class="col col-sm-3 btn-danger">Column</div>
             <div class="col col-sm-6 btn-info">Column</div>
             <div class="col col-sm-3 btn-primary">Column</div>
         </div>
-    </div>
+    </div>-->
    
-<!-- ***************************************************INICIO DE AREA DE TRABAJO******************************--> 
+<!-- ***************************************************INICIO DE AREA DE TRABAJO****************************************************--> 
     <div class="container-fluid">
-        <h1 class="">Datos Personales</h1>
+        <h1 class="text-center font-weight-light">Datos Personales</h1><br>
         <div class="container-fluid">     
         <div class="row">
             <div class="col-sm-3  ">
@@ -81,67 +89,105 @@
                 <h6><strong>Cargo: </strong><span class="badge badge-dark"><?php echo $row1['descripcion'];?></span></h5>
                 <h6><strong>Cedula:</strong> <?php echo $row['empleado_id'];?></h5>
                 <div class="btn-group">
-                    <a href="../tip_cons/planta.php" class="btn btn-primary ">ATRAS</a>
+                    <a href="../tip_cons/planta.php" class="btn btn-outline-primary ">ATRAS</a>
                     <?php if($row1['estado_id']=="1"){ ?>
-                    <a href="../operaciones/modificar_planta.php?id_empleado=<?php echo $row['empleado_id']?>" class="btn btn-primary">MODIFICAR</a>
+                    <a href="../operaciones/modificar_planta.php?id_empleado=<?php echo $row['empleado_id']?>" class="btn btn-outline-primary">MODIFICAR</a>
                     <a class="btn btn-primary disabled">ACTIVO</a>
                     <?php } else { ?>
                                      
-                        <a class="btn btn-danger  text-dark" data-toggle="popover" title="Datos de Inactividad" data-content="Renuncia el  <?php echo $row1['fecha_vencimiento']; ?> ">INACTIVO</a>
+                        <a class="btn btn-outline-danger  text-dark" data-toggle="popover" title="Datos de Inactividad" data-content="Renuncia el  <?php echo $row1['fecha_vencimiento']; ?> ">INACTIVO</a>
                     <?php } ?>
 
                 </div>
             </div>
             <div class="col-sm-6 ">
-                <div class=" container">
-                    <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo">DATOS PERSONALES</button>
-                    <div id="demo" class="collapse">
-                        <a class="list-group-item list-group-item-success" >                              <big><strong>Ciudad Nacimiento:    </strong></big> <?php echo $row1['ciudad_nacimiento']; ?></a>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6 ">
+                            <div class=" container">
+                                <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo">CONTRATO</button>
+                                <div id="demo" class="collapse">
+                                    <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Contrato/Fecha Ingreso:    </strong></big> <?php echo $row1['fecha_ingreso']; ?></a>
+                                    <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Contrato/Fecha Vencimiento:    </strong></big> <?php echo $row1['fecha_vencimiento']; ?></a>
+                                    <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Tipo Contrato:    </strong></big> <?php echo $row5['descripcion']; ?></a>
+                                    <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>N° Prorogas:    </strong></big> <?php echo $row4['prorrogas']; ?></a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-sm-6 ">
+                            <div class=" container-fluid ">
+                                <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo2">CAPACITACION</button>
+                                <div id="demo2" class="collapse">
+                                    <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Academia:    </strong></big> <?php echo $row2['nombre']; ?></a>
+                                    <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Nivel curso:    </strong></big> <?php echo $row2['descripcion']; ?></a>
+                                    <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Vencimiento Curso:    </strong></big> <?php echo $row2['fecha_vencimiento']; ?></a> 
+                                    <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Fecha de Acreditacion:    </strong></big> <?php echo $row['fecha_acreditacion']; ?></a>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+
+<!--00 ********************************************************EFECTO DE IMAGENES*****************************************************-->
+                <div class="container mt-3">
+
+                    
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                      <!-- Indicators -->
+                      <ul class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                      </ul>
+                      
+                      <!-- The slideshow -->
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="../<?php echo $ruta1; ?>" alt="Los Angeles" width="600" height="500">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="../<?php echo $ruta2; ?>" alt="Chicago" width="600" height="500">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="../<?php echo $ruta3; ?>" alt="New York" width="600" height="500">
+                        </div>
+                      </div>
+                      
+                      <!-- Left and right controls -->
+                      <a class="carousel-control-prev" href="#myCarousel" data-slide="prev" >
+                        <span class="carousel-control-prev-icon"></span>
+                      </a>
+                      <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                      </a>
+                    </div>
+                </div>
+<!--00 *******************************************************FIN EFECTO DE IMAGENES**************************************************-->
+            </div>
+            <div class="col-sm-3">
+                <div class=" container p-2 bg-info">
+                    <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo1">DATOS PERSONALES</button>
+                    <div >
+                        <a class="list-group-item list-group-item-info"><big><strong>Ciudad Nacimiento:    </strong></big> <?php echo $row1['ciudad_nacimiento']; ?></a>
                         <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Expedicion Cedula:    </strong></big> <?php echo $row1['ciudad_expedicion']; ?></a>
                         <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Ciudad/Residencia:  </strong></big> <?php echo $row1['ciudad_residencia']; ?></a>
                         <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action" ><big><strong>Direccion/Residencia:    </strong></big> <?php echo $row1['direccion']; ?></a>
                         <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Telefono:    </strong></big> <?php echo $row1['telefono']; ?></a>
                         <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Tipo de Sangre:    </strong></big> <?php echo $row1['tipo_sangre']; ?></a>
-                        <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Contrato/Fecha Ingreso:    </strong></big> <?php echo $row1['fecha_ingreso']; ?></a>
-                        <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Contrato/Fecha Vencimiento:    </strong></big> <?php echo $row1['fecha_vencimiento']; ?></a>
-                        <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Nivel curso:    </strong></big> <?php echo $row2['descripcion']; ?></a>
-                        <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Vencimiento Curso:    </strong></big> <?php echo $row2['fecha_vencimiento']; ?></a> 
-                        <a class="list-group-item list-group-item-info" class="list-group-item list-group-item-action"><big><strong>Fecha de Acreditacion:    </strong></big> <?php echo $row['fecha_acreditacion']; ?></a>
+                      
+                       
                     </div>
                 </div>
-            <!-- ***************************************************EFECTO DE IMAGENES******************************-->
-                <div id="demo" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item ">
-                            <img src="../<?php echo $ruta1; ?>" alt="Los Angeles" width="600" height="500">
-                            <div class="carousel-caption">
-                                <h3><kbd>Cedula</h3>
-                            </div>   
-                        </div>
-                        <div class="carousel-item active">
-                            <img src="../<?php echo $ruta2; ?>" alt="Chicago" width="600" height="500">
-                            <div class="carousel-caption">
-                                <h3><kbd>Credencial</h3>
-                            </div>   
-                        </div>
-                        <div class="carousel-item ">
-                            <img src="../<?php echo $ruta3; ?>" alt="New York" width="600" height="500">
-                            <div class="carousel-caption">
-                                <h3><kbd>Curso</h3>
-                            </div>   
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-3">
-                <iframe src="https://www.supervigilancia.gov.co/?idcategoria=61193"  width="304" height="550">
-                    <p>Your browser does not support iframes.</p>
-                </iframe>
+                
         
             </div>
         </div>
     </div>
-<!-- ***************************************************FIN DE AREA DE TRABAJO******************************-->    
+<!-- ***************************************************FIN DE AREA DE TRABAJO******************************************************-->    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -154,6 +200,6 @@
     </script>
 </body>
 </html>
-<!--************************************************************** FIN AREA DE DISEÑO (FRONTEND)PROYECTO *****************-->
+<!--************************************************************** FIN AREA DE DISEÑO (FRONTEND)PROYECTO *****************************-->
 
 
